82f04f08339d78ba2764fd3c02d3abaa78f107d8
Nishant Bindal <nishant.bindal@nagarro.com>
2019-05-28 14:51:00 +0530

    build trigger

--- MongoDbDemo/Controllers/HomeController.cs
+++ MongoDbDemo/Controllers/HomeController.cs
@@ -1,16 +1,10 @@
-﻿using MongoDbDemo.Providers;
-using MongoDbDemo.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MongoDbDemo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index() 
         {
             //MongoDbTutorialsTopicsService mongoDbTutorialsIndexService = new MongoDbTutorialsTopicsService(new MongoDbConfigProvider());
             return View();
